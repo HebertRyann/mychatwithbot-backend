4b196c1a17de3d9c93c05c25c7fc1a75c8e22be8
HebertRyann <hebertryann40@gmail.com>
2021-03-31 23:43:03 -0300

    Test upload for heroku

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const server = require('http').createServer(app);
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
+    origin: "https://chatwithbotteste.herokuapp.com",
     methods: ["GET", "POST"]
   }
 });
