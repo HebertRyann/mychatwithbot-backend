d6cda0c16cd029a21034b7bdabf2046483a6fe33
HebertRyann <hebertryann40@gmail.com>
2021-03-31 23:34:47 -0300

    Test upload for heroku

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*'
+  }
+});
+
+let currentRoom = '';
+let socketId = '';
+let users = [];
+
+io.on('connection', (socket) => {
+  socket.on('join', ({ name, room }) => {
+    users.push({ id: `${socket.id}`, name });
+    socket.join(`${room}`);
+    currentRoom = `${room}`;
+    console.log(users);
+    io.to(room).emit('newUser', name);
+    io.emit('Users', users);
+  });
+  socket.on('sendMessage', ({ message, name }) => {
+    io.to(currentRoom).emit('ReceivedMessage', { name, message });
+  });
+  socket.on('SendPrivateMessage', ({ socketID, message }) => {
+    socket.join(`${socketID}`);
+    socketId = `${socketID}`;
+    io.to(socketId).emit('ReceivedMessage', { name: 'Jose', message });
+  });
+});
+
+http.listen(3333, () => {
+  console.log('Server Started on Port 3333')
+});
